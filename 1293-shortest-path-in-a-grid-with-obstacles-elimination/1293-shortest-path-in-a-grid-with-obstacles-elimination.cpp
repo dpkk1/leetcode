d00fa67972f244d4c4a95e1bf63dbe85318d103b
Deepak Kumar <62848415+dpkk1@users.noreply.github.com>
2022-05-24 08:09:52 +0530

    Time: 382 ms (19.03%), Space: 50.8 MB (24.19%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();    // No. of rows
-        int n = grid[0].size(); // No. of cols
-        vector<vector<int>> visited(m, vector<int>(n,-1));
-        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>vis(m,vector<int>(n, -1));
         queue<vector<int>>q;
-        
-        //x, y, currentLength, remaining k
-        q.push({0, 0, 0, k});
-        
+        q.push({0,0,0,k});
         while(!q.empty()){
             auto t = q.front();
             q.pop();
-            int x=t[0], y=t[1];
-            
+            int x = t[0], y=t[1];
             if(x<0 || x>=m || y<0 || y>=n)
                 continue;
             
-            //Reached the destination? return the currentLength
-            if(x==m-1 && y==n-1)
+            if(x==m-1 && y==n-1){
                 return t[2];
-            
-            //Hit the obstacle
+            }
             if(grid[x][y]==1){
-                if(t[3] > 0)
+                if(t[3]>0)
                     t[3]--;
                 else
                     continue;
             }
             
-            if(visited[x][y] != -1 && visited[x][y] >= t[3])
+            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
                 continue;
-            
-            visited[x][y] = t[3];
-            
+            vis[x][y] = t[3];
             q.push({x+1, y, t[2]+1, t[3]});
             q.push({x-1, y, t[2]+1, t[3]});
             q.push({x, y+1, t[2]+1, t[3]});
